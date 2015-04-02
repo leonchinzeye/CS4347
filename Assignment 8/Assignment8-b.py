36498dff5f63e02b8b7e99a6631029be72780422
leonchinzeye <leonchin499@gmail.com>
2015-04-02 19:50:39 +0800

    Added Assignment 8 files

--- Assignment 8/Assignment8-b.py
+++ Assignment 8/Assignment8-b.py
@@ -29,11 +29,39 @@ def main():
 	perfect_sine_wave_freq1 = calcSineWave(freq1)
 	perfect_sine_wave_freq2 = calcSineWave(freq2)
 
-	plotWave(freq1_vanilla_size1, freq1_linear_size1, perfect_sine_wave_freq1, "100.0Hz - LUT size = 16384", "100.0Hz - LUT size = 16384.png")
-	plotWave(freq1_vanilla_size2, freq1_linear_size2, perfect_sine_wave_freq1, "100.0Hz - LUT size = 2048", "100.0Hz - LUT size = 2048.png")
+	err1 = np.max(np.abs(freq1_vanilla_size1 - perfect_sine_wave_freq1[:size1]))
+	err2 = np.max(np.abs(freq1_vanilla_size2 - perfect_sine_wave_freq1[:size2]))
+	err3 = np.max(np.abs(freq1_linear_size1 - perfect_sine_wave_freq1[:size1]))
+	err4 = np.max(np.abs(freq1_linear_size2 - perfect_sine_wave_freq1[:size2]))
 
-	plotWave(freq2_vanilla_size1, freq2_linear_size1, perfect_sine_wave_freq2, "1234.56Hz - LUT size = 16384", "1234.56Hz - LUT size = 16384.png")
-	plotWave(freq2_vanilla_size2, freq2_linear_size2, perfect_sine_wave_freq2, "1234.56Hz - LUT size = 2048", "1234.56Hz - LUT size = 2048.png")
+	err5 = np.max(np.abs(freq2_vanilla_size1 - perfect_sine_wave_freq2[:size1]))
+	err6 = np.max(np.abs(freq2_vanilla_size2 - perfect_sine_wave_freq2[:size2]))
+	err7 = np.max(np.abs(freq2_linear_size1 - perfect_sine_wave_freq2[:size1]))
+	err8 = np.max(np.abs(freq2_linear_size2 - perfect_sine_wave_freq2[:size2]))
+
+	err1 = 32767 * err1
+	err2 = 32767 * err2
+	err3 = 32767 * err3
+	err4 = 32767 * err4
+	err5 = 32767 * err5
+	err6 = 32767 * err6
+	err7 = 32767 * err7
+	err8 = 32767 * err8
+
+	writeFile(err1, err2, err3, err4, err5, err6, err7, err8)
+	# plotWave(freq1_vanilla_size1, freq1_linear_size1, perfect_sine_wave_freq1, "100.0Hz - LUT size = 16384", "100.0Hz - LUT size = 16384.png")
+	# plotWave(freq1_vanilla_size2, freq1_linear_size2, perfect_sine_wave_freq1, "100.0Hz - LUT size = 2048", "100.0Hz - LUT size = 2048.png")
+
+	# plotWave(freq2_vanilla_size1, freq2_linear_size1, perfect_sine_wave_freq2, "1234.56Hz - LUT size = 16384", "1234.56Hz - LUT size = 16384.png")
+	# plotWave(freq2_vanilla_size2, freq2_linear_size2, perfect_sine_wave_freq2, "1234.56Hz - LUT size = 2048", "1234.56Hz - LUT size = 2048.png")
+
+def writeFile(err1, err2, err3, err4, err5, err6, err7, err8):
+	newFile = open("max_audio_file_error.txt", "w")
+	newFile.write("Frequency\tInterpolation\t16384-sample\t\t\t2048-sample\n")
+	newFile.write("100Hz\t\tNo\t\t\t\t" + str(err1) + "\t\t\t" + str(err2) + "\n")
+	newFile.write("\t\t\tLinear\t\t\t" + str(err3) + "\t\t" + str(err4) + "\n")
+	newFile.write("1234.56Hz\tNo\t\t\t\t" + str(err5) + "\t\t\t" + str(err6) + "\n")
+	newFile.write("\t\t\tLinear\t\t\t" + str(err7) + "\t\t" + str(err8) + "\n")
 
 def plotWave(data1, data2, data3, title, fileName):
 	plot.figure(figsize = (18, 8))
@@ -64,15 +92,10 @@ def useLutGetLinear(sample_size, freq):
 	return genBufferOutputLinear(sample_size, delta_phi, lut)
 
 def genBufferOutputVanilla(sample_size, delta_phi, lut):
-	phase = 0.0
 	buff = np.zeros(sample_size)
 
 	for i in range(sample_size):
-		buff[i] = lut[int(phase)]
-		phase += delta_phi
-
-		if phase >= sample_size:
-			phase %= sample_size
+		buff[i] = lut[round(i * delta_phi) % sample_size]
 
 	return buff
 
@@ -81,9 +104,9 @@ def genBufferOutputLinear(sample_size, delta_phi, lut):
 	
 	for i in range(sample_size):
 		x0 = np.floor(i * delta_phi % sample_size)
-		x1 = (x0 + 1) % sample_size
-		y0 = lut[x0]
-		y1 = lut[x1]
+		x1 = (x0 + 1)
+		y0 = lut[x0 % sample_size]
+		y1 = lut[x1 % sample_size]
 
 		result = np.append(result, y0 + (y1 - y0) * ((i * delta_phi % sample_size) - x0) / (x1 - x0))
 
